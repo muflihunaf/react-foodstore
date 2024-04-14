5bac917bf18342cbb7e6c180d45b56155565f40b
muflihunaf <firmanedam19@gmail.com>
2024-04-15 06:58:55 +0700

    [FEAT] register

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,52 +1,69 @@
 import * as React from 'react';
-import { Button, Card, FormControl, InputText, LayoutOne } from 'upkit';
-
+import { Button, Card, FormControl, InputText, LayoutOne, InputPassword } from 'upkit';
+import { useForm } from 'react-hook-form';
+import { rules } from './validation';
 export default function Register(){
+
+  let { register, handleSubmit, errors, setError } = useForm();
+
+  const onSubmit = async formData => {
+    console.log(errors);
+
+  }
+
   return (
     <LayoutOne size="small">
-        <Card color="white">
-          <form>
-            <FormControl>
-            <InputText 
-            name='full_name'
-            placeholder='Nama Lengkap'
-            fitContainer
-            />
-            </FormControl>
 
-            <FormControl>
-            <InputText 
-            name='email'
-            placeholder='Email'
-            fitContainer
-            />
-            </FormControl>
+       <Card color="white">
+         <form onSubmit={handleSubmit(onSubmit)}>
 
-            <FormControl>
-            <InputText 
-            name='password'
-            placeholder='Password'
-            fitContainer
-            />
-            </FormControl>
+           <FormControl errorMessage={errors.full_name?.message}>
+             <InputText
+               name="full_name"
+               placeholder="Nama Lengkap"
+               fitContainer 
+               ref={register(rules.full_name)}
+             />
+           </FormControl>
 
-            <FormControl>
-            <InputText 
-            name='password_confirmation'
-            placeholder='Konfirmasi Password'
-            fitContainer
-            />
-            </FormControl>
+           <FormControl errorMessage={errors.email?.message}>
+             <InputText 
+               name="email"
+               placeholder="Email"
+               fitContainer
+               ref={register(rules.email)}
+             />
+           </FormControl>
 
-            <Button
-            size='large'
-            fitContainer
-            color='blue'
-            >
-            Daftar
-            </Button>
-          </form>
-        </Card>
+           <FormControl errorMessage={errors.password?.message}>
+             <InputPassword
+               name="password"
+               placeholder="Password"
+               fitContainer
+               ref={register(rules.password)}
+             />
+           </FormControl>
+
+           <FormControl errorMessage={errors.password_confirmation?.message}>
+             <InputPassword 
+               name="password_confirmation"
+               placeholder="Konfirmasi Password"
+               fitContainer
+               ref={register(rules.password_confirmation)}
+             />
+           </FormControl>
+
+					 <Button
+					   size="large" 
+					   fitContainer
+					 > Daftar</Button>
+
+
+         </form>
+
+
+       </Card>
+
     </LayoutOne>
   )
 }
