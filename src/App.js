53d11851bae40ca57542d45a1ed97baa836f9833
muflihunaf <firmanedam19@gmail.com>
2024-04-29 20:12:41 +0700

    guard, account, order

--- src/App.js
+++ src/App.js
@@ -1,48 +1,69 @@
-
-import { HashRouter as Router, Route, Switch } from 'react-router-dom'
+import React from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import 'upkit/dist/style.min.css';
 import Home from './pages/Home';
-import { Provider } from 'react-redux';
-import store from './app/store';
-import React from 'react';
 import { listen } from './app/listener';
+import { Provider } from 'react-redux';
+
+import GuestOnlyRoute from './components/GuestOnlyRoute';
+import GuardRoute from './components/GuardRoute';
+import Logout from './pages/Logout';
+import UserOrders from './pages/UserOrders';
+import UserAccount from './pages/UserAccount';
+import Invoice from './pages/Invoice';
+import Checkout from './pages/Checkout';
+import UserAddress from './pages/UserAdddress';
+import UserAddressAdd from './pages/UserAddressAdd';
 import Register from './pages/Register';
 import RegisterSuccess from './pages/RegisterSuccess';
 import Login from './pages/Login';
-import { getCart } from './api/cart';
-import UserAddressAdd from './pages/UserAddressAdd';
-import UserAddress from './pages/UserAdddress';
-import Checkout from './pages/Checkout';
-import Invoice from './pages/Invoice';
+import store from './app/store';
+import {getCart} from './api/cart';
+
 function App() {
 
   React.useEffect(() => {
     listen();
     getCart();
-  },[])
+  }, []);
 
   return (
     <Provider store={store}>
       <Router>
         <Switch>
-        <Route path="/login">
-          <Login/>
-          </Route>
-          <Route path="/register/berhasil" component={RegisterSuccess} />
-          <Route path="/register" component={Register} />
-          <Route path="/alamat-pengiriman/tambah">
-            <UserAddressAdd/>
-          </Route>
-          <Route path="/alamat-pengiriman">
-            <UserAddress/>
-          </Route>
-          <Route path="/checkout">
-            <Checkout/>
-          </Route>
-          <Route path="/invoice/:order_id">
+          <GuardRoute path="/logout">
+            <Logout/>
+          </GuardRoute>
+          <GuardRoute path="/pesanan">
+            <UserOrders/>
+          </GuardRoute>
+          <GuardRoute path="/account">
+            <UserAccount/>
+          </GuardRoute>
+          <GuardRoute path="/invoice/:order_id">
             <Invoice/>
+          </GuardRoute>
+          <GuardRoute path="/checkout">
+            <Checkout/>
+          </GuardRoute>
+          <GuardRoute path="/alamat-pengiriman/tambah">
+            <UserAddressAdd/>
+          </GuardRoute>
+          <GuardRoute path="/alamat-pengiriman">
+            <UserAddress/>
+          </GuardRoute>
+          <GuestOnlyRoute path="/register/berhasil">
+            <RegisterSuccess/>
+          </GuestOnlyRoute>
+          <GuestOnlyRoute path="/register">
+            <Register/>
+          </GuestOnlyRoute>
+          <GuestOnlyRoute path="/login">
+            <Login/>
+          </GuestOnlyRoute>
+          <Route path="/">
+            <Home/>
           </Route>
-          <Route path="/"  component={Home} />
         </Switch>
       </Router>
     </Provider>
